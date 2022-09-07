0205a34c54c2cef5083dc1e05aeec098cf26eabf
ZEMONG2 <zerosole0@gmail.com>
2022-09-07 18:39:57 +0900

    save

---
+++ front-end/src/component/route/RouterComponent.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import UserListComponent from "../user/UserListComponent";
+import AddUserComponent from "../user/AddUserComponent";
+import EditUserComponent from "../user/EditUserComponent";
+
+const AppRouter = () => {
+  return (
+    <div style={style}>
+      <Routes>
+        <Route exact path="/" element={<UserListComponent />} />
+        <Route path="/users" element={<UserListComponent />} />
+        <Route path="/add-user" element={<AddUserComponent />} />
+        <Route path="/edit-user" element={<EditUserComponent />} />
+      </Routes>
+    </div>
+  );
+};
+
+const style = {
+  marginTop: "20px",
+};
+
+export default AppRouter;
